a0a12438ebb71e6738d1bd6f7b09202b061e3bc8
ryujiin <ryujiin22@gmail.com>
2013-11-01 12:21:37 -0700

    commit form

--- loviz/contab/admin.py
+++ loviz/contab/admin.py
@@ -4,6 +4,13 @@ from models import *
 class EnlaceCliente(admin.ModelAdmin):
 	list_display= ('nombre','direccion','ruc')
 
+class CuentaCliente(admin.ModelAdmin):
+	list_display=('cliente','deuda','abono','total','fecha')
+
+class Materiales_lista(admin.ModelAdmin):
+	list_display=('nombre','precio',)	
 
 admin.site.register(Cliente , EnlaceCliente)
-admin.site.register(Cuenta_Cliente)
+admin.site.register(Cuenta_Cliente, CuentaCliente)
+admin.site.register(Modelos_calzado)
+admin.site.register(Material,Materiales_lista)
--- loviz/contab/models.py
+++ loviz/contab/models.py
@@ -15,3 +15,19 @@ class Cuenta_Cliente(models.Model):
 	abono = models.IntegerField(default = 0)
 	total = models.IntegerField(default=0)
 	fecha = models.DateTimeField(auto_now_add=True)
+
+class Material(models.Model):
+	nombre = models.CharField(max_length = 140)
+	precio = models.IntegerField(default = 0)
+
+	def __str__(self):
+		return self.nombre
+
+class Modelos_calzado(models.Model):
+	nombre = models.CharField(max_length=140)
+	materiales = models.ForeignKey(Material)
+	costo = models.IntegerField(default=0)
+	
+	def __str__(self):
+		return self.nombre
+
